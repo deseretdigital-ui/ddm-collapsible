fbada4ae40de300de00414d6dec1b0c982b5253a
Joshua Stoutenburg <jehoshua02@gmail.com>
2014-11-04 16:37:55 -0700

    Update 2014-11-04T23:37:53.068Z

--- collapsible.js
+++ collapsible.js
@@ -1,76 +1,260 @@
-// collapsible init function
-var ddm = ddm || {};
-ddm.collapsible = (function ($) {
+var CollapsibleHead = React.createClass({displayName: 'CollapsibleHead',
 
-  // collapsible constructor
-  var Collapsible = function ($element, options) {
+  /* react hooks */
 
-    var collapsible = this;
-    $element.addClass('ddm-collapsible');
+  propTypes: {
+    href: React.PropTypes.string
+  },
 
-    // wrap the body
-    var $content = $element.find(options.body);
-    $content.wrap('<div class="ddm-collapsible__body"></div>');
-    var $body = $element.find('.ddm-collapsible__body');
+  getDefaultProps: function() {
+    return {
+      href: null
+    }
+  },
 
-    // bind head as toggle
-    var $head = $element.find(options.head);
-    $head.addClass('ddm-collapsible__head');
-    $head.on('click.ddm.collapsible', function (event) {
-      $element.trigger('toggle.ddm.collapsible');
-    });
+  render: function () {
+    return (
+      React.createElement("div", {className: "ddm-collapsible__head", onClick: this.props.onClick}, 
+        this.renderChildren(), 
+        React.createElement("span", {className: "ddm-collapsible__arrow"})
+      )
+    );
+  },
 
-    // add arrow
-    if (options.arrow !== false) {
-      $head.addClass('ddm-collapsible__head--with-arrow');
+  renderChildren: function () {
+    var children;
+
+    if (this.props.href) {
+      children = (React.createElement("a", {href: this.props.href}, this.props.children));
+    } else {
+      children = this.props.children;
     }
 
-    collapsible.isOpen = function () {
-      return $element.hasClass('ddm-collapsible--open');
-    };
+    return children;
+  }
 
-    $element.on('open.ddm.collapsible', function (event) {
-      var height = $body.children().eq(0).outerHeight(true);
-      $body.css('max-height', height);
-      $element.addClass('ddm-collapsible--open');
-    });
+});
 
-    $element.on('close.ddm.collapsible', function (event) {
-      $body.css('max-height', '');
-      $element.removeClass('ddm-collapsible--open');
-    });
+var CollapsibleBody = React.createClass({displayName: 'CollapsibleBody',
+  render: function () {
+    return (
+      React.createElement("div", {className: "ddm-collapsible__body"}, 
+        this.props.children
+      )
+    );
+  }
+});
 
-    $element.on('toggle.ddm.collapsible', function (event) {
-      if (collapsible.isOpen()) {
-        $element.trigger('close.ddm.collapsible');
-      } else {
-        $element.trigger('open.ddm.collapsible');
-      }
-    });
+var Collapsible = React.createClass({displayName: 'Collapsible',
 
-    $element.on('teardown.ddm.collapsible', function (event) {
-      $element.removeClass('ddm-collapsible ddm-collapsible--open');
-      $content.unwrap();
-      $head.removeClass('ddm-collapsible__head ddm-collapsible__head--with-arrow');
-      $head.off('.ddm.collapsible');
-      $element.off('.ddm.collapsible');
-      $element.removeData('ddm-collapsible-api');
-    });
+  /* react hooks */
 
-    return collapsible;
-  };
-
-
-
-  var collapsible = function ($element, options) {
-    $element = $element.eq(0); // only handles one collapsible at a time
-    var api = $element.data('ddm-collapsible-api');
-    if (!api) {
-      api = new Collapsible($element, options);
-      $element.data('ddm-collapsible-api', api);
+  getInitialState: function() {
+    return {
+      mounted: false,
+      open: false || this.props.open,
+      opening: false,
+      closing: false
     }
-    return api;
-  };
+  },
 
-  return collapsible;
-})(jQuery);
+  render: function () {
+    return (
+      React.createElement("div", {className: this.getClassNames()}, 
+        this.renderChildren()
+      )
+    );
+  },
+
+  componentDidMount: function () {
+    this.setMaxHeight();
+    setTimeout(function () {
+      this.setState({mounted: true});
+    }.bind(this), 0);
+
+  },
+
+  componentDidUpdate: function () {
+    this.setMaxHeight();
+  },
+
+
+
+  /* event handlers */
+
+  handleHeadClick: function (event) {
+    this.toggle();
+  },
+
+
+
+  /* methods */
+
+  open: function () {
+    this.setState({
+      open: true,
+      opening: true
+    });
+
+    setTimeout(function () {
+      this.setState({
+        opening: false
+      });
+    }.bind(this), 300); /* this is brittle; it assumes the transition duration is .3s */
+
+    if (this.props.onOpen) {
+      this.props.onOpen(this);
+    }
+  },
+
+  close: function () {
+    this.setState({
+      closing: true
+    });
+
+    setTimeout(function () {
+      this.setState({
+        closing: false,
+        open: false
+      });
+    }.bind(this), 300);
+  },
+
+  toggle: function () {
+    if (this.state.open) {
+      this.close();
+    } else {
+      this.open();
+    }
+  },
+
+
+
+  /* helpers */
+
+  getClassNames: function () {
+    return React.addons.classSet({
+      'ddm-collapsible': true,
+      'ddm-collapsible--mounted': this.state.mounted,
+      'ddm-collapsible--open': this.state.open
+    });
+  },
+
+  setMaxHeight: function () {
+    var body = this.refs.body.getDOMNode();
+    var child = body.children[0];
+    var maxHeight;
+
+    if (this.state.opening || this.state.closing) {
+      /* we need something to transition to or from */
+      maxHeight = child.offsetHeight + 'px';
+    } else if (this.state.open) {
+      /* lest there be nested collapsibles, remove max-height */
+      maxHeight = 'none';
+    } else {
+      /* unset to make closed */
+      maxHeight = null;
+    }
+
+    body.style.maxHeight = maxHeight;
+  },
+
+  renderChildren: function () {
+    return React.Children.map(this.props.children, this.renderChild);
+  },
+
+  renderChild: function (child, index) {
+    if (child.type === CollapsibleHead.type) {
+      return this.renderHead(child);
+    } else if (child.type === CollapsibleBody.type) {
+      return this.renderBody(child);
+    } else {
+      return child;
+    }
+  },
+
+  renderHead: function (child) {
+    return React.addons.cloneWithProps(child, {
+      ref: 'head',
+      onClick: this.handleHeadClick
+    });
+  },
+
+  renderBody: function (child) {
+    return React.addons.cloneWithProps(child, {
+      ref: 'body'
+    });
+  }
+
+});
+
+var CollapsibleGroup = React.createClass({displayName: 'CollapsibleGroup',
+
+  /* react hooks */
+
+  propTypes: {
+    accordion: React.PropTypes.bool,
+    open: React.PropTypes.bool
+  },
+
+  getDefaultProps: function () {
+    return {
+      accordion: false,
+      open: false
+    }
+  },
+
+  render: function () {
+    return (
+      React.createElement("div", {className: "ddm-collapsible-group"}, 
+        this.renderChildren()
+      )
+    );
+  },
+
+
+
+  /* event handlers */
+
+  handleCollapsibleOpen: function (collapsible) {
+    if (this.props.accordion) {
+      this.closeOtherCollapsibles(collapsible);
+    }
+  },
+
+
+
+  /* methods */
+
+
+
+  /* helpers */
+
+  closeOtherCollapsibles: function (collapsible) {
+    for (var key in this.refs) {
+      var ref = this.refs[key];
+      var isCollapsible = ref.type !== Collapsible.type;
+      var isSame = ref.props.index === collapsible.props.index;
+      if (!isCollapsible || isSame) { continue; }
+      ref.close();
+    }
+  },
+
+  renderChildren: function () {
+    return React.Children.map(this.props.children, this.renderChild);
+  },
+
+  renderChild: function (child, index) {
+    if (child.type !== Collapsible.type) { return child; }
+    child = React.addons.cloneWithProps(child, {
+      key: 'ddmCollapsible' + index,
+      ref: 'ddmCollapsible' + index,
+      index: index,
+      open: child.props.open === undefined ? this.props.open : child.props.open,
+      onOpen: this.handleCollapsibleOpen
+    });
+
+    return child;
+  }
+
+});
