f623c553bc7f6510510ebd665a0b8f2a8689100d
Joshua Stoutenburg <jehoshua02@gmail.com>
2014-11-09 03:00:49 -0700

    Fix Bugs by Tightening Control on Timing
    Introduces a polling technique in the `after()` method to only
    move to the next state after expected conditions are satisfied.

--- src/jsx/Collapsible.jsx
+++ src/jsx/Collapsible.jsx
@@ -3,26 +3,30 @@ var Collapsible = React.createClass({
   propTypes: {
     open: React.PropTypes.bool,
     onOpen: React.PropTypes.func,
-    onClose: React.PropTypes.func
+    onClose: React.PropTypes.func,
+    speed: React.PropTypes.number
   },
 
   getDefaultProps: function () {
     return {
       open: null,
       onOpen: function () {},
-      onClose: function () {}
+      onClose: function () {},
+      speed: 800 /* pixels per second */
     }
   },
 
   getInitialState: function() {
-    console.log(this.props.open);
     return {
-      open: (this.props.open || false)
+      open: this.props.open || false,
+      willOpen: false,
+      opening: false,
+      willClose: false,
+      closing: false
     }
   },
 
   render: function () {
-    console.log('render');
     return (
       <div className={this.getClassNames()}>
         {this.renderChildren()}
@@ -33,7 +37,12 @@ var Collapsible = React.createClass({
   getClassNames: function () {
     return React.addons.classSet({
       'ddm-collapsible': true,
-      'ddm-collapsible--open': this.state.open
+      'ddm-collapsible--will-open': this.state.willOpen,
+      'ddm-collapsible--opening': this.state.opening,
+      'ddm-collapsible--open': this.state.open,
+      'ddm-collapsible--will-close': this.state.willClose,
+      'ddm-collapsible--closing': this.state.closing,
+      'ddm-collapsible--closed': this.isClosed()
     });
   },
 
@@ -60,103 +69,226 @@ var Collapsible = React.createClass({
 
   renderBody: function (child) {
     return React.addons.cloneWithProps(child, {
-      ref: 'body'
+      ref: 'body',
+      key: 'body'
     });
   },
 
-  componentDidMount: function () {
-    this.setInitialHeight();
-  },
-
-  componentDidUpdate: function (prevProps, prevState) {
-    if (!prevState.open && this.state.open) { /* opening */
-      this.transitionToHeightAuto();
-    } else if (prevState.open && !this.state.open) { /* closing */
-      this.transitionFromHeightAuto();
-    }
-  },
-
-  setInitialHeight: function () {
-    var body = this.refs.body.getDOMNode();
-    var height = this.state.open ? 'auto' : '0';
-    body.style.height = height;
-  },
-
   handleHeadClick: function (event) {
-    console.log('click');
-    this.toggle();
-  },
-
-  open: function () {
-    if (this.state.open) { return; }
-    this.props.onOpen(this);
-    this.setState({ open: true });
-  },
-
-  close: function () {
-    if (!this.state.open) { return; }
-    this.props.onClose(this);
-    this.setState({ open: false });
+    setTimeout(function () {
+      this.toggle();
+    }.bind(this), 0)
   },
 
   toggle: function () {
     if (this.state.open) {
       this.close();
-    } else {
+    } else if (this.isClosed()) {
       this.open();
     }
   },
 
-  handleTransitionEnd: function (event) {
-    console.log('transitionend');
-    if (event.propertyName == 'height') {
-      var body = this.refs.body.getDOMNode();
-      this.removeClass(body, 'ddm-collapsible__body--transition');
-      body.style.height = 'auto';
-      body.removeEventListener('transitionend', this.handleTransitionEnd, false);
+  open: function () {
+    if (this.inTransition() || this.state.open) {
+      return; /* nothing to do or already doing something */
     }
+
+    this.props.onOpen(this);
+
+    this.prepareOpen();
   },
 
-  transitionToHeightAuto: function () { /* encapsulate messy stuff */
-    console.log('transitionToHeightAuto');
-    var body = this.refs.body.getDOMNode();
-    body.style.height = '0px';
-    this.addClass(body, 'ddm-collapsible__body--transition');
-    body.style.height = this.getContentHeight();
-    body.addEventListener('transitionend', this.handleTransitionEnd, false);
+  prepareOpen: function () {
+    this.setState({ willOpen: true }, function () {
+      this.after(this.hasWillOpenClass, this.startOpen);
+    }.bind(this));
   },
 
-  setHeightZero: function () {
-    var body = this.refs.body.getDOMNode();
-    this.addClass(body, 'ddm-collapsible__body--transition');
-    body.style.height = '0px';
+  hasWillOpenClass: function () {
+    return this.hasClass(this.getDOMNode(), 'ddm-collapsible--will-open');
   },
 
-  transitionFromHeightAuto: function () { /* encapsulate messy stuff */
-    var body = this.refs.body.getDOMNode();
-    body.style.height = this.getContentHeight();
-    setTimeout(this.setHeightZero, 0);
+  startOpen: function () {
+    this.transitionEnd(this.finishOpen);
+    this.setState({ willOpen: false, opening: true }, function () {
+      this.after(this.hasOpeningClass, this.setBodyHeight);
+    }.bind(this));
+  },
+
+  hasOpeningClass: function () {
+    return this.hasClass(this.getDOMNode(), 'ddm-collapsible--opening');
+  },
+
+  finishOpen: function () {
+    this.setState({ opening: false, open: true }, this.unsetBodyHeight);
+  },
+
+  close: function () {
+    if (this.inTransition() || this.isClosed()) {
+      return; /* nothing to do or already doing something */
+    }
+
+    this.props.onClose(this);
+
+    this.prepareClose();
+  },
+
+  prepareClose: function () {
+    this.setBodyHeight();
+    this.setState({ open: false, willClose: true }, this.startClose);
+  },
+
+  startClose: function () {
+    this.transitionEnd(this.finishClose);
+    this.setState({ willClose: false, closing: true }, function () {
+      this.after(this.readyToClose, this.unsetBodyHeight);
+    });
+  },
+
+  readyToClose: function () {
+    var ready = (
+      this.hasClosingClass()
+      && this.hasBodyHeight()
+    );
+    return ready;
+  },
+
+  finishClose: function () {
+    this.setState({ closing: false });
+  },
+
+  isClosed: function () {
+    return !(
+      this.state.open
+      || this.inTransition()
+    );
+  },
+
+  inTransition: function () {
+    return (
+      this.state.willOpen
+      || this.state.opening
+      || this.state.willClose
+      || this.state.closing
+    );
+  },
+
+  setBodyHeight: function () {
+    this.refs.body.getDOMNode().style.height = this.getContentHeight();
+  },
+
+  setTransitionDuration: function () {
+    var contentHeight = parseInt(this.getContentHeight(), 10);
+    var duration = contentHeight / this.props.speed;
+    var styleName = this.transitionStyleName() + 'Duration';
+    this.refs.body.getDOMNode().style[styleName] = duration + 's';
+  },
+
+  unsetBodyHeight: function () {
+    this.refs.body.getDOMNode().style.height = null;
   },
 
   getContentHeight: function () {
-    console.log(this.refs.body.refs.content.getDOMNode().offsetHeight);
     return this.refs.body.refs.content.getDOMNode().offsetHeight + 'px';
   },
 
-  addClass: function (element, className) {
-    if (element.classList) {
-      element.classList.add(className);
-    } else {
-      element.className += ' ' + className;
+  transitionEnd: function (callback) {
+    var eventName = this.transitionEndEventName();
+    var body = this.refs.body.getDOMNode();
+    var handler = function (event) {
+      if (event.propertyName === 'height') {
+        callback();
+        body.removeEventListener(eventName, handler);
+      }
+    };
+
+    body.addEventListener(eventName, handler, false);
+  },
+
+  transitionSupported: function () {
+    return this.transitionEndEventName() !== false;
+  },
+
+  transitionEndEventName: function () {
+    /* adapted from https://github.com/facebook/react/blob/master/src/addons/transitions/ReactTransitionEvents.js */
+
+    var eventNames = {
+      'transition': 'transitionend',
+      'WebkitTransition': 'webkitTransitionEnd',
+      'MozTransition': 'mozTransitionEnd',
+      'OTransition': 'oTransitionEnd',
+      'msTransition': 'MSTransitionEnd'
+    };
+
+    if (!('TransitionEvent' in window)) {
+      delete eventNames['transition'];
+    }
+
+    var style = document.createElement('div').style;
+    var styleName = this.transitionStyleName();
+    var eventName = false;
+
+    if (styleName !== false) {
+      eventName = eventNames[styleName];
+    }
+
+    return eventName;
+  },
+
+  transitionStyleName: function () {
+    var styleNames = [
+      'transition',
+      'WebkitTransition',
+      'MozTransition',
+      'OTransition',
+      'msTransition'
+    ];
+
+    var style = document.createElement('div').style;
+    var styleName = false;
+
+    for (var i = 0; i < styleNames.length; i++) {
+      var key = styleNames[i];
+      if (key in style) {
+        styleName = key;
+        break;
+      }
+    }
+
+    return styleName;
+  },
+
+  after: function (check, action, limit) {
+    limit = limit === undefined ? 10 : limit;
+    if (check()) {
+      action();
+    } else if (limit > 1) {
+      setTimeout(function () {
+        this.after(check, action, --limit);
+      }.bind(this), 0);
     }
   },
 
-  removeClass: function (element, className) {
+  hasClosingClass: function () {
+    var hasClosingClass = this.hasClass(this.getDOMNode(), 'ddm-collapsible--closing');
+    return hasClosingClass;
+  },
+
+  hasBodyHeight: function () {
+    var bodyHeight = this.refs.body.getDOMNode().style.height;
+    var contentHeight = this.getContentHeight();
+    var hasBodyHeight = bodyHeight === contentHeight;
+    return hasBodyHeight;
+  },
+
+  hasClass: function (element, className) {
+    var hasClass = false;
     if (element.classList) {
-      element.classList.remove(className);
+      hasClass =  element.classList.contains(className);
     } else {
-      element.className = element.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+      hasClass = new RegExp('(^| )' + className + '( |$)', 'gi').test(element.className);
     }
+    return hasClass;
   }
 
 });
