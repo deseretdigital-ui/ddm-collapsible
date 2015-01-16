02bb64cb363e76372f54ac7da82a93a63b45068c
Kate Bratko <kbratko@softserveinc.com>
2015-01-16 18:43:45 +0200

    Minify isie8 script

--- src/util/isIE8.js
+++ src/util/isIE8.js
@@ -1,8 +1,4 @@
 module.exports = (function () {
-  var IE8 = false;
-  if (navigator.userAgent.search('MSIE 8') > 0) {
-    IE8 = true;
-    return IE8;
-  }
-}());
+  return navigator.userAgent.search('MSIE 8') > 0;
+})();
 
