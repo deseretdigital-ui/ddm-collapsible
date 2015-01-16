c35bd3f0790467ac91e1868f0f5a55eda79bd9bf
Kate Bratko <kbratko@softserveinc.com>
2015-01-16 12:13:18 +0200

    Update isie8 check script

--- src/util/isIE8.js
+++ src/util/isIE8.js
@@ -1,8 +1,8 @@
-(function () {
+module.exports = (function () {
   var IE8 = false;
-  if (navigator.userAgent.search('Chrome') > 0) {
+  if (navigator.userAgent.search('MSIE 8') > 0) {
     IE8 = true;
     return IE8;
   }
-})
+}());
 
