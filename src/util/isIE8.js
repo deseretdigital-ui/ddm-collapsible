fbc254e0f3b632d336b2513de32e0f6c6a1526e5
Kate Bratko <kbratko@softserveinc.com>
2015-01-16 11:20:16 +0200

    Update isIE function and some refatoring

--- src/util/isIE8.js
+++ src/util/isIE8.js
@@ -1,10 +1,8 @@
-var isIE8 = function () {
+(function () {
   var IE8 = false;
-  if (navigator.userAgent.search('MSIE 8') > 0) {
+  if (navigator.userAgent.search('Chrome') > 0) {
     IE8 = true;
     return IE8;
   }
-}
-
- module.exports = isIE8;
+})
 
